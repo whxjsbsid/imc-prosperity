700e6a66f0e378c18cf35cb912016692b2c86b7a
Justin Ho <juntingho4@gmail.com>
2026-04-25 10:19:29 +0800

    Update r3_main.py

--- r3/r3_main.py
+++ r3/r3_main.py
@@ -12,7 +12,7 @@ class Trader:
     # Trade only when price is meaningfully away from fair value.
     HYDROGEL_FAIR = 9990.8
     HYDROGEL_EDGE = 31.9*0.9
-    HYDROGEL_MAX_TAKE_SIZE = 20
+    HYDROGEL_MAX_TAKE_SIZE = 40
     HYDROGEL_FLATTEN_THRESHOLD = 80
     HYDROGEL_FLATTEN_SIZE = 10
 
